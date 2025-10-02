c49158460e319586332041f3964d8490f30e484d
EmirB457 <balijagicemir5@gmail.com>
2025-10-02 11:19:57 +0200

    Add files via upload

---
+++ Vjezbe2.java
@@ -0,0 +1,48 @@
+package Vjezbe;
+
+import java.util.Scanner;
+public class Vjezbe2 {
+
+	public static void main(String[] args) {
+		/*
+		
+		Scanner sc= new Scanner (System.in);
+		int N=sc.nextInt();
+		long D=sc.nextLong();
+		long HD=sc.nextLong();
+		long KA=sc.nextLong();
+		int brojprijetnji=0;
+		for (int i=0;i<N;i++) {
+			long x=sc.nextLong();
+			long y=sc.nextLong();
+			
+			long menheten=Math.abs(x)+Math.abs(y);
+			
+					if(menheten <= D) {
+						brojprijetnji++;
+						
+						
+						
+						
+					}
+			
+			
+			
+		}
+		long totaldamage = brojprijetnji * KA;
+		System.out.println("Nas dvorac ukupno ugrozava %f",brojprijetnji);
+		if(totaldamage>HD) {
+			System.out.println("Dvorac je srusen,ukupan damage je: "+ totaldamage);
+			
+		} else {
+			
+		}
+		System.out.println("Dvorac je srusen");
+		*/
+		
+		
+				
+		
+	}
+
+}
